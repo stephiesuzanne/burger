94df9fa88306ca82dfaa091815b113d4c295b5e3
Stephanie Davis <stephiesuzanne@outlook.com>
2020-01-07 14:10:30 +0800

    config

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+connection.connect();
+module.exports = connection;
+© 2020 GitHub, Inc.
